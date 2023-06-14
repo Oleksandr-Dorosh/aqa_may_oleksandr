f17e853a19716f24d2470947ef5b7af6316300bf
Oleksandr-Dorosh <131899201+Oleksandr-Dorosh@users.noreply.github.com>
2023-06-14 13:23:29 +0300

    homework 7

---
+++ homework7/src/main/java/org/homework/TaskCalculator.java
@@ -0,0 +1,65 @@
+package org.homework;
+
+import java.util.Scanner;
+
+public class TaskCalculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num1 = getInt();
+        int num2 = getInt();
+        char operation = getOperation();
+        int result = calc(num1, num2, operation);
+        System.out.println("Результат операції:" + result);
+    }
+
+
+    public static int getInt() {
+        System.out.println("Введіть число:");
+        int num;
+        if (scanner.hasNextInt()) {
+            num = scanner.nextInt();
+        } else {
+            System.out.println("помилка");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+
+    }
+
+    public static char getOperation() {
+        System.out.println("Введіть операцію:");
+        char operation;
+        if (scanner.hasNext()) {
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Помилка");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static int calc(int num1, int num2, char operation) {
+        int result;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Невідома операція");
+                result = calc(num1, num2, getOperation());
+        }
+        return result;
+    }
+}
